14990af567fa6af28c2be48d1a29595e8c0c504d
hjq1015 <yong.huang1@thoughtworks.com>
2021-10-21 09:37:55 +0800

    添加docker配置文件

--- user/src/main/java/com/thoughtworks/mapper/UserMapper.java
+++ user/src/main/java/com/thoughtworks/mapper/UserMapper.java
@@ -12,13 +12,13 @@ import java.util.List;
 public interface UserMapper {
     List<UserEntity> queryUserList(@Param("user") UserQueryRequest user);
 
-    void addUser(@Param("user") UserOperateRequest user);
+    Integer addUser(@Param("user") UserOperateRequest user);
 
-    void updateUserEmail(@Param("userId") Long userId, @Param("email") String email);
+    Integer updateUserEmail(@Param("userId") Long userId, @Param("email") String email);
 
-    void updateUser(@Param("user") UserOperateRequest user);
+    Integer updateUser(@Param("user") UserOperateRequest user);
 
-    void deleteUser(@Param("userId") Long userId);
+    Integer deleteUser(@Param("userId") Long userId);
 
     UserEntity queryUserByID(@Param("userId") Long userId);
 
